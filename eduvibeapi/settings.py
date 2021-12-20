753ef564f678789c6f7a3c508215c10b7bcbcb2f
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-12-20 18:19:43 +0530

    deploy

--- eduvibeapi/settings.py
+++ eduvibeapi/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://eduvibe.herokuapp.com/']
 
 
 # Application definition
